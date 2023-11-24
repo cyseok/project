3cd108c73d3999d6655c2b1a8dada04dbff3b5ee
cyseok <cysuk123@naver.com>
2023-11-25 04:36:00 +0900

    11/25 네이버 지역 검색 API 구현

--- project/src/main/java/com/project/controller/NaverSearchController.java
+++ project/src/main/java/com/project/controller/NaverSearchController.java
@@ -1,12 +1,14 @@
 package com.project.controller;
 
+import java.io.UnsupportedEncodingException;
 import java.net.URI;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.nio.charset.Charset;
 
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
@@ -15,75 +17,87 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 
 @RestController
-@RequestMapping("/naver")
-public class NaverSearch {
+@RequestMapping(value="/naver")
+public class NaverSearchController {
 	
-	@GetMapping("/search")
-	public ResponseEntity<JSONObject> naverSearchList(@RequestParam String text
-            , @RequestParam(defaultValue = "1") int start
-            , @RequestParam(defaultValue = "5") int display
-            , @RequestParam(defaultValue = "random") String sort) throws JsonMappingException, JsonProcessingException, ParseException {
+	@GetMapping(value = "/search", produces = "application/json; charset=UTF-8")
+	public ResponseEntity<String> naverSearchList(@RequestParam(name = "text") String searchText) throws UnsupportedEncodingException  {
 		
-		// 네이버 검색 API 요청
 		String clientId = "lLK4tKRRKZzB3p_z8n5e"; 		
         String clientSecret = "H3jKWKwxmm";
         
-        // https://openapi.naver.com/v1/search/local.json?
-        //          query=프롬상록&display=10&start=1&sort=random
-        String uri = UriComponentsBuilder
+        // https://openapi.naver.com/v1/search/local.json?query=경복궁&display=5&start=1&sort=random
+        // String decodedText = URLDecoder.decode(searchText, "UTF-8");
+        // String encodedText = URLEncoder.encode(searchText, "UTF-8");
+        
+        URI uri = UriComponentsBuilder
       		  .fromUriString("https://openapi.naver.com")
       		  .path("/v1/search/local.json")
-      		  .queryParam("query", text)
-      		  .queryParam("display", 10)
+      		  .queryParam("query", searchText)
+      		  .queryParam("display", 5)
       		  .queryParam("start", 1)
       		  .queryParam("sort", "random")
-      		  .toUriString();
+      		  .encode(Charset.forName("UTF-8"))
+      		  .build()
+              .toUri();
         
-        // 방법 1
-        WebClient webClient = WebClient.builder()
-        		.baseUrl(uri)
-        		// .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-        		.defaultHeader("X-Naver-Client-Id", clientId)
-        		.defaultHeader("X-Naver-Client-Secret", clientSecret)
-        		.build();
-        
-        String responseBody = webClient.get()
-        		.retrieve()
-        		.bodyToMono(String.class)
-        		.block();
-        
-        //JSONParser 객체 : JSON 형식의 문자열을 JSON 객체로 변환
-  		JSONParser parser=new JSONParser();
-  		//JSONParser.parse(String json) : JSON 형식의 문자열을 Object 객체로 변환
-  		Object object=parser.parse(responseBody);
-  		//Object 객체로 JSONObject 객체로 변환하여 저장
-  		JSONObject jsonObject=(JSONObject)object;
-  		
-        System.out.println("Naver Search API Response: " + responseBody);
-        
-        // 방법 2
-        /*
-        RestTemplate restTemplate = new RestTemplate();
-        
+        // 방법1
         RequestEntity<Void> req = RequestEntity
                 .get(uri)
                 .header("X-Naver-Client-Id", clientId)
                 .header("X-Naver-Client-Secret", clientSecret)
+                .header("Content-Type", "application/json; charset=UTF-8")
                 .build();
+        
+        RestTemplate restTemplate = new RestTemplate();
         
         ResponseEntity<String> responseEntity = restTemplate.exchange(req, String.class);
         
         String responseBody = responseEntity.getBody();
-        */
         
-        return ResponseEntity.ok(jsonObject);
+        /*
+        // 방법 2
+        HttpHeaders headers = new HttpHeaders();
+        	headers.set("X-Naver-Client-Id", clientId);
+        	headers.set("X-Naver-Client-Secret", clientSecret);
+            headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
+        
+        RestTemplate restTemplate = new RestTemplate();
+        
+        ResponseEntity<String> responseEntity = restTemplate.exchange(uri, HttpMethod.GET, requestEntity, String.class);
+        
+        String responseBody = responseEntity.getBody();
+         */
+        
+        
+        /* 방법 3
+        WebClient webClient = WebClient.builder()
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .defaultHeader("X-Naver-Client-Id", clientId)
+                .defaultHeader("X-Naver-Client-Secret", clientSecret)
+                .build();
+
+        ResponseEntity<String> responseEntity = webClient.get()
+                .uri(uri)
+                .retrieve()
+                .toEntity(String.class)
+                .block();
+                
+        String responseBody = responseEntity.getBody();
+         */
+        
+        
+        System.out.println("API Response: " + responseBody);
+        System.out.println("API searchText: " + searchText);
+        //System.out.println("API encodedText: " + encodedText);
+        System.out.println("API uri: " + uri);
+        
+        return ResponseEntity.ok(responseBody);
 	}
-	
 }
