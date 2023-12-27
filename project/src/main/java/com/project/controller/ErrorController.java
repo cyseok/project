303bc3bff5f9e3bced2723e8f3a6d0abd06548c2
cyseok <cysuk123@naver.com>
2023-12-27 09:53:57 +0900

    권한이 없을때 이동되는 에러 페이지

---
+++ project/src/main/java/com/project/controller/ErrorController.java
@@ -0,0 +1,17 @@
+package com.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Controller
+public class ErrorController {
+	
+	@GetMapping("/error")
+	public String diyAdd() {
+		return "/error";
+	}
+
+}
