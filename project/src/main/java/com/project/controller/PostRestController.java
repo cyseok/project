9a6025e8e486bfcf595cedb26695ce8bb809f41a
cyseok <cysuk123@naver.com>
2024-01-06 21:09:22 +0900

    게시글 컨트롤러 (에디터 등록 설정)

--- project/src/main/java/com/project/controller/PostRestController.java
+++ project/src/main/java/com/project/controller/PostRestController.java
@@ -4,6 +4,9 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.CollectionModel;
@@ -23,7 +26,18 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.multipart.MultipartRequest;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import com.project.dto.Likes;
 import com.project.dto.Post;
@@ -43,6 +57,9 @@ public class PostRestController {
 	
 	@Autowired
 	private final LikesService likesService;
+	
+	@Autowired
+	private final WebApplicationContext context; 
 	
 	@GetMapping(value = "/list")
 	public ResponseEntity<CollectionModel<Post>> getPostList(
@@ -224,6 +241,38 @@ public class PostRestController {
 		}
 	}
 	
+	
+	
+	@PostMapping(value = "/image-upload")
+	public ResponseEntity<?> imageUpload(@RequestParam("file") MultipartFile file) throws IllegalStateException, IOException {
+		try {
+			
+			// 서버에 저장할 경로
+			String uploadDirectory = context.getServletContext().getRealPath("/resources/assets/images/upload"); 
+			
+			// 업로드 된 파일의 이름
+			String originalFileName = file.getOriginalFilename();
+			
+			// 업로드 된 파일의 확장자
+			String fileExtension = originalFileName.substring(originalFileName.lastIndexOf("."));
+			
+			// 업로드 될 파일의 이름 재설정 (중복 방지를 위해 UUID 사용)
+			String uuidFileName = UUID.randomUUID().toString() + fileExtension;
+			
+			// 위에서 설정한 서버 경로에 이미지 저장
+			file.transferTo(new File(uploadDirectory, uuidFileName));
+		
+			System.out.println("************************ 업로드 컨트롤러 실행 ************************");
+			System.out.println(uploadDirectory);
+			
+			// Ajax에서 업로드 된 파일의 이름을 응답 받을 수 있도록 해줍니다.
+			return ResponseEntity.ok(uuidFileName);
+		} catch (Exception e) {
+			return ResponseEntity.badRequest().body("이미지 업로드 실패");
+		}
+		
+	}
+
 	// 게시글 등록 
 	@PostMapping(produces = "application/json")
 	public ResponseEntity<Post> postAdd(Post post) {
